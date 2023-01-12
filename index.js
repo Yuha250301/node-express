8b38e35d9cd5a5294d0c71c1700372dc4ae82bfb
Truong Nguyen Anh Huy <huytnase150173@fpt.edu.vn>
2023-01-13 00:01:24 +0700

    Express Simple REST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"),
     http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 5000;
@@ -9,6 +10,13 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.all("/nations", (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+});
 
 app.use((req, res, next) => {
     console.log(req.headers);
